ce0783753825b7a1de05000bf57f35fba7448080
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-27 19:58:14 -0700

    implement login functionality and add navigation

--- app/routes.py
+++ app/routes.py
@@ -17,3 +17,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
