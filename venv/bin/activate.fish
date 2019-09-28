df208ead84a29ded10c50227e443f58040f85962
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-28 15:41:21 -0700

    reorganize file system folders

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/MyFolder/Desktop/microblog/venv"
+set -gx VIRTUAL_ENV "/Users/MyFolder/Downloads/microblog-master/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
