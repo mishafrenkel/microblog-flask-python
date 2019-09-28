00411d62d8891cb1adc8d2ee7b9cd070c4c237a5
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-27 20:01:48 -0700

    improve field validation

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-from app import routes
 from flask import Flask
 from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
+# app.config.from_object(Config)
+from app import routes
